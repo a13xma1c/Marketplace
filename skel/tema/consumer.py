967dcd52ebcbba931456b41052af76c8cb6e137a
a13xma1c <alexmcn07@gmail.com>
2021-03-28 15:03:25 +0300

    added comments

--- skel/tema/consumer.py
+++ skel/tema/consumer.py
@@ -37,15 +37,21 @@ class Consumer(Thread):
         self.carts = carts
         self.marketplace = marketplace
         self.retry_wait_time = retry_wait_time
+        # initialize the consumer with the given name and set it as a daemon thread
         super().__init__(name=kwargs.get("name"), daemon=kwargs.get("daemon"))
 
     def run(self):
+        # register a single cart for the consumer
         cart_id = self.marketplace.new_cart()
+        # coalesce carts to a single cart entity
         all_ops = reduce(operator.add, self.carts)
+        # iterate over operations
         for single_op in all_ops:
+            # produce as many items as specified
             quantity = single_op.get("quantity")
             while quantity > 0:
                 if single_op.get("type") == "add":
+                    # check if a product can be added to the cart. if not, wait and retry
                     status = False
                     while not status:
                         status = self.marketplace.add_to_cart(cart_id, single_op.get("product"))
@@ -53,8 +59,10 @@ class Consumer(Thread):
                             break
                         time.sleep(self.retry_wait_time)
                 elif single_op.get("type") == "remove":
+                    # remove a product from the cart
                     self.marketplace.remove_from_cart(cart_id, single_op.get("product"))
                 quantity -= 1
 
+        # print the final contents of the cart
         for prod in self.marketplace.place_order(cart_id):
             print(self.name + " bought " + prod.__repr__())
--- skel/tema/producer.py
+++ skel/tema/producer.py
@@ -35,14 +35,20 @@ class Producer(Thread):
         self.products = products
         self.marketplace = marketplace
         self.republish_wait_time = republish_wait_time
+        # initialize the producer with the given name and set it as a daemon thread
         super().__init__(name=kwargs.get("name"), daemon=kwargs.get("daemon"))
 
     def run(self):
+        # register producer
         producer_id = self.marketplace.register_producer()
+        # continuously add products to the marketplace
         while True:
+            # iterate over the list of available products
             for product in self.products:
+                # add as many products as specified by the quantity element of the product tuple
                 quantity = product[1]
                 while quantity > 0:
+                    # check if the producer queue is full. if it is, wait and retry
                     status = False
                     while not status:
                         status = self.marketplace.publish(producer_id, product[0])
